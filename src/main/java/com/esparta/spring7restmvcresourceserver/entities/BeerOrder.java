c5656a7f4a99a930de2dfd9719deadb33a83138e
MartinMSalas <ingmartinmsalas@gmail.com>
2026-04-21 09:22:51 -0300

    feat<BeerOrder>:impl

---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/entities/BeerOrder.java
@@ -0,0 +1,106 @@
+package com.esparta.spring7restmvcresourceserver.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+/*
+ * Author: M
+ * Date: 09-Feb-26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+@Entity
+@Table(
+        name = "beer_order"
+
+)
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+public class BeerOrder {
+
+    public BeerOrder(UUID beerOrderId, String customerRef, Customer customer, Set<BeerOrderLine> beerOrderLines,
+                     BeerOrderShipment beerOrderShipment, Instant createdDate, Instant lastModifiedDate, Long version ) {
+
+        this.beerOrderId = beerOrderId;
+        this.customerRef = customerRef;
+        this.setCustomer(customer);
+        this.beerOrderLines = beerOrderLines;
+        this.beerOrderShipment = beerOrderShipment;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.version = version;
+    }
+
+    // =========================================================
+    // PRIMARY KEY (technical) - internal identifier
+    // =========================================================
+    @Id
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(name = "beer_order_id", columnDefinition = "CHAR(36)", nullable = false, updatable = false)
+    private UUID beerOrderId;
+
+    @Column(name = "customer_ref")
+    private String customerRef;
+
+    // =========================================================
+    // RELATIONSHIPS
+    // =========================================================
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "beerOrder", cascade = CascadeType.PERSIST)
+    private Set<BeerOrderLine> beerOrderLines = new HashSet<>();
+
+    @OneToOne(mappedBy = "beerOrder", cascade = CascadeType.PERSIST)
+    private BeerOrderShipment beerOrderShipment;
+
+    // =========================================
+    // Bidirectional sync logic
+    // =========================================
+
+    public void setCustomer(Customer customer) {
+
+        this.customer = customer;
+        customer.getBeerOrders().add(this);
+
+    }
+    // =========================================================
+    // AUDITING
+    // =========================================================
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private Instant createdDate;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date", nullable = false)
+    private Instant lastModifiedDate;
+
+    // =========================================================
+    // OPTIMISTIC LOCKING
+    // =========================================================
+    @Version
+    @Column(name = "version")
+    private Long version;
+
+
+
+
+}
+
---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/model/BeerDTO.java
@@ -0,0 +1,83 @@
+package com.esparta.spring7restmvcresourceserver.model;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+/*
+ * Author: M
+ * Date: 24-Jan-26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+
+    /* =========================
+       IDENTITY
+       ========================= */
+
+    private UUID beerId;
+    /**
+     * External identifier (CSV, supplier, legacy system, etc.)
+     * Optional – may be null.
+     */
+    private Integer externalBeerId;
+
+
+    /* =========================
+       DOMAIN DATA
+       ========================= */
+
+    @NotBlank
+    @Size(max = 255)
+    private String beerName;
+
+    @NotNull
+    private BeerStyle beerStyle;
+
+    @NotBlank
+    @Size(max = 50)
+    private String upc;
+
+    @NotNull
+    @Min(0)
+    private Integer quantityOnHand;
+
+    @NotNull
+    @DecimalMin("0.00")
+    private BigDecimal price;
+
+    /* =========================
+       AUDITING (READ-ONLY)
+       ========================= */
+
+    private Instant createdDate;
+    private Instant lastModifiedDate;
+
+    /* =========================
+       OPTIMISTIC LOCKING
+       ========================= */
+
+    private Long version;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BeerDTO beerDTO = (BeerDTO) o;
+        return Objects.equals(beerId, beerDTO.beerId) && Objects.equals(externalBeerId, beerDTO.externalBeerId) && Objects.equals(beerName, beerDTO.beerName) && beerStyle == beerDTO.beerStyle && Objects.equals(upc, beerDTO.upc) && Objects.equals(quantityOnHand, beerDTO.quantityOnHand) && Objects.equals(price, beerDTO.price) && Objects.equals(version, beerDTO.version);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(beerId, externalBeerId, beerName, beerStyle, upc, quantityOnHand, price, version);
+    }
+}
