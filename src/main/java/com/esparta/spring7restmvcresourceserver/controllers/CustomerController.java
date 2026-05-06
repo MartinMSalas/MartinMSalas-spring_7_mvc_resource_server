1e52e6391762b1a2aefb7e80b0d18ed278b2c0f4
MartinMSalas <ingmartinmsalas@gmail.com>
2026-05-06 17:36:07 -0300

    feat<CustomerController>:update

---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/controllers/CustomerController.java
@@ -0,0 +1,113 @@
+package com.esparta.spring7restmvcresourceserver.controllers;
+
+import com.esparta.spring7restmvcresourceserver.exceptions.BadRequestException;
+import com.esparta.spring7restmvcresourceserver.model.CustomerDTO;
+import com.esparta.spring7restmvcresourceserver.services.CustomerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+/*
+ * Author: M
+ * Date: 25-Jan-26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+@AllArgsConstructor
+@RestController
+@RequestMapping(CustomerController.CUSTOMER_PATH)
+@Slf4j
+public class CustomerController {
+
+    public static final String CUSTOMER_PATH = "/api/v1/customer";
+    public static final String CUSTOMER_PATH_ID = "/{customerId}";
+
+    private static final String HEADER_TOTAL_COUNT = "X-Total-Count";
+
+    private final CustomerService customerService;
+
+    @PostMapping()
+    public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO){
+        log.debug("In CustomerController.createNewCustomer() with customerDTO: {}", customerDTO);
+        CustomerDTO savedCustomerDTO = customerService.saveNewCustomer(customerDTO);
+        log.debug("Saved CustomerDTO: {}", savedCustomerDTO);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location","/api/v1/customer/" + savedCustomerDTO.getCustomerId().toString());
+
+        return new ResponseEntity<>(savedCustomerDTO, headers, org.springframework.http.HttpStatus.CREATED);
+    }
+
+    @GetMapping(CUSTOMER_PATH_ID)
+    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable UUID customerId){
+        log.debug("In CustomerController.getCustomerById() with id: {}", customerId);
+        CustomerDTO customerDTO = customerService.getCustomerById(customerId);
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("location", "/api/v1/customer/" + customerDTO.getCustomerId().toString());
+        return new ResponseEntity<>(customerDTO, headers, HttpStatus.OK);
+    }
+
+    /**
+     * Returns all customers.
+     *
+     * Contract:
+     * - 200 OK with list (possibly empty)
+     * - Never returns null
+     */
+    @GetMapping()
+    public ResponseEntity<List<CustomerDTO>> getAllCustomers(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "25") Integer size
+            // @RequestParam(required = false) Integer size
+    ) {
+
+        log.debug("GET /api/v1/customers?page={}, size={}", page, size);
+
+        if ((page < 0) || ( size <= 0) || size > 100) {
+            throw new BadRequestException("Invalid pagination parameters");
+        }
+
+        // For now, ignoring pagination parameters
+        List<CustomerDTO> customers = customerService.getAllCustomers();
+        // Add X-Total-Count header if pagination is implemented in the future
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache().mustRevalidate());
+        headers.add(HEADER_TOTAL_COUNT, String.valueOf(customers.size()));
+        return new ResponseEntity<>(customers, headers, HttpStatus.OK);
+    }
+
+    @PutMapping(CUSTOMER_PATH_ID)
+    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable UUID customerId, @RequestBody CustomerDTO customerDTO) {
+        log.debug("In CustomerController.updateCustomer() with id: {}", customerId);
+        CustomerDTO customerDTOUpdated = customerService.updateCustomer(customerId, customerDTO);
+        log.debug("Updated CustomerDTO: {}", customerDTOUpdated);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + customerDTOUpdated.getCustomerId().toString());
+        return new ResponseEntity<>(customerDTOUpdated, headers, org.springframework.http.HttpStatus.OK);
+    }
+
+    @DeleteMapping(CUSTOMER_PATH_ID)
+    public ResponseEntity<CustomerDTO> deleteCustomer(@PathVariable UUID customerId) {
+        log.debug("In CustomerController.deleteCustomer() with id: {}", customerId);
+
+        CustomerDTO customerDTODeleted = customerService.deleteCustomer(customerId);
+        return new ResponseEntity<>(customerDTODeleted, HttpStatus.OK);
+    }
+
+    @PatchMapping(CUSTOMER_PATH_ID)
+    public ResponseEntity<CustomerDTO> patchCustomer(@PathVariable UUID customerId, @RequestBody CustomerDTO customerDTO) {
+        log.debug("In CustomerController.patchCustomer() with id: {}", customerId);
+        CustomerDTO customerDTOPatched = customerService.patchCustomer(customerId, customerDTO);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + customerDTOPatched.getCustomerId().toString());
+        return new ResponseEntity<>(customerDTOPatched, headers, HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/esparta/spring7restmvcresourceserver/controllers/CustomerControllerTest.java
@@ -0,0 +1,226 @@
+package com.esparta.spring7restmvcresourceserver.controllers;
+
+import com.esparta.spring7restmvcresourceserver.configuration.JpaAuditingConfig;
+
+import com.esparta.spring7restmvcresourceserver.exceptions.NotFoundException;
+import com.esparta.spring7restmvcresourceserver.model.CustomerDTO;
+
+import com.esparta.spring7restmvcresourceserver.services.CustomerService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+
+import java.util.List;
+
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(
+        controllers = CustomerController.class,
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = JpaAuditingConfig.class
+        )
+)
+class CustomerControllerTest {
+
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockitoBean
+    CustomerService customerService;
+
+    public static final String CUSTOMER_PATH = "/api/v1/customer";
+    public static final String CUSTOMER_PATH_ID = CUSTOMER_PATH + "/{customerId}";
+
+    static CustomerDTO customerDTO;
+    static List<CustomerDTO> customersList;
+
+    @BeforeAll
+    static void setUp() {
+        customerDTO = CustomerDTO.builder()
+                .customerId(UUID.randomUUID())
+                .customerName("PoNCio")
+                .build();
+        customersList = List.of(customerDTO);
+    }
+
+    @Test
+    void givenValidCustomerPayload_whenPostCustomer_thenReturn201Created() throws Exception {
+        // GIVEN a valid customer payload
+        UUID customerId = customerDTO.getCustomerId();
+        given(customerService.saveNewCustomer(any(CustomerDTO.class))).willReturn(customerDTO);
+
+        // WHEN the POST endpoint is called
+        mockMvc.perform(post(CUSTOMER_PATH)
+
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customerDTO)))
+                // THEN the response status is 201 CREATED
+
+                .andExpect(status().isCreated())
+                // AND the Location header points to the new resource
+
+                .andExpect(header().string("Location", "/api/v1/customer/" + customerId.toString()));
+    }
+
+    @Test
+    void  givenValidCustomerId_whenGetCustomerById_thenReturn200OkAndCustomer() throws Exception {
+        // GIVEN valid customer ID
+        UUID customerId = customerDTO.getCustomerId();
+        // AND the service returns the customerDTO
+
+
+        given(customerService.getCustomerById(eq(customerId))).willReturn(customerDTO);
+
+        // WHEN the GET-by-ID endpoint is called
+        mockMvc.perform(get(CUSTOMER_PATH_ID,  customerId)
+                        .accept(MediaType.APPLICATION_JSON))
+                // THEN the response status is 200 OK
+
+                .andExpect(status().isOk())
+                // AND the response is JSON
+
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // AND the returned beer matches the expected values
+
+                .andExpect(jsonPath("$.customerId",is(customerId.toString())))
+                .andExpect(jsonPath("$.customerName", is("PoNCio")));
+    }
+
+    @Test
+    void givenCustomersExist_whenGetBeers_thenReturn200OkAndSingleBeer() throws Exception {
+
+        // GIVEN a list of customers returned by the service
+        given(customerService.getAllCustomers()).willReturn(customersList);
+        // WHEN the GET collection endpoint is called
+        mockMvc.perform(get(CUSTOMER_PATH)
+                        .accept(MediaType.APPLICATION_JSON))
+                // THEN the response status is 200 OK
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()",is(1)));
+
+    }
+
+    @Test
+    void givenNonExistingCustomerId_whenGetCustomerById_thenReturn404NotFound() throws Exception {
+
+        // GIVEN the service throws NotFoundException
+        given(customerService.getCustomerById(any(UUID.class))).willThrow(NotFoundException.class);
+
+        // WJHEN the GET-by-ID endpoint is called
+        mockMvc.perform(get(CUSTOMER_PATH_ID, UUID.randomUUID()))
+                // THEN the response status is 404 Not Found
+                .andExpect(status().isNotFound());
+
+
+    }
+
+    @Test
+    void givenValidUpdatedCustomerPayload_whenUpdateCustomer_thenReturn200OkAndCustomer() throws Exception {
+        // GIVEN a valid updated customer payload
+        UUID customerId = customerDTO.getCustomerId();
+        given(customerService.updateCustomer(eq(customerDTO.getCustomerId()), any(CustomerDTO.class))).willReturn(customerDTO);
+
+        // WHEN the PUT endpoint is called
+
+        mockMvc.perform(put(CUSTOMER_PATH_ID,  customerId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customerDTO)))
+                // THEN the response status is 200 OK
+                .andExpect(status().isOk())
+                // AND the Location header is returned
+                .andExpect(header().string("Location", "/api/v1/customer/" + customerId.toString()));
+
+        //verify(customerService, times(1)).updateCustomer(any(UUID.class), any(CustomerDTO.class));
+        // OR use ArgumentCaptor
+        verify(customerService).updateCustomer(eq(customerId), any(CustomerDTO.class));
+    }
+
+    @Test
+    void givenValidPatchCustomerPayload_whenPatchCustomer_thenReturn200Ok() throws Exception {
+        // GIVEN a valid patched customer payload
+        UUID customerId = customerDTO.getCustomerId();
+        CustomerDTO patchedCustomer = CustomerDTO.builder()
+                .customerName("PatchedName")
+                .build();
+        given(customerService.patchCustomer(eq(customerId), any(CustomerDTO.class))).willReturn(customerDTO);
+
+        // WHEN the PATCH endpoint is called
+        mockMvc.perform(patch(CUSTOMER_PATH_ID, customerId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patchedCustomer)))
+                // THEN the response status is 200 OK
+                .andExpect(status().isOk());
+
+        // Verify that the service method was called with correct parameters
+        verify(customerService).patchCustomer(eq(customerId), any(CustomerDTO.class));
+    }
+
+    @Test
+    void givenValidCustomerId_whenDeleteCustomer_thenReturn200Ok() throws Exception {
+        // given
+        UUID customerId = customerDTO.getCustomerId();
+        given(customerService.deleteCustomer(eq(customerId))).willReturn(customerDTO);
+
+
+        // when & then
+        mockMvc.perform(delete(CUSTOMER_PATH_ID, customerId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);
+        verify(customerService).deleteCustomer(uuidArgumentCaptor.capture());
+        UUID capturedUuid = uuidArgumentCaptor.getValue();
+        // assert(capturedUuid.equals(customerId));
+        assertEquals(customerId, capturedUuid);
+        verify(customerService, times(1)).deleteCustomer(customerId);
+    }
+
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void createNewCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+
+    @Test
+    void patchCustomer() {
+    }
+}
