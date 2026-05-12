ceff8bf6f6cca9614bb7bcf7fe0355a46c32fc71
MartinMSalas <ingmartinmsalas@gmail.com>
2026-05-12 19:24:55 -0300

    feat<CustomerDTO>:impl

---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/model/CustomerDTO.java
@@ -0,0 +1,42 @@
+package com.esparta.spring7restmvcresourceserver.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/*
+ * Author: M
+ * Date: 25-Jan-26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+
+    private UUID customerId;
+    @NotBlank(message = "Customer name must not be blank")
+    @Size(max = 255, message = "Customer name must be at most 255 characters")
+    private String customerName;
+    /* =========================
+    AUDITING
+    ========================= */
+    private Instant createdDate;
+
+    private Instant lastModifiedDate;
+
+    /* =========================
+       OPTIMISTIC LOCKING
+       Used for optimistic locking (copied from entity, enforced by JPA)
+    ========================= */
+    private Long version;
+
+}
