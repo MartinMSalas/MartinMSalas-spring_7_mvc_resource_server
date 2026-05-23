20250e294f60cc7b2b85ea9ac590cfe63ea316ab
MartinMSalas <ingmartinmsalas@gmail.com>
2026-05-23 11:03:41 -0300

    update<JPAAud>:impl

---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/configuration/JpaAuditingConfig.java
@@ -0,0 +1,15 @@
+package com.esparta.spring7restmvcresourceserver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/*
+ * Author: m
+ * Date: 26/1/26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
