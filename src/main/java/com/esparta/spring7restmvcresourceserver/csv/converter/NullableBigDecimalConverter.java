00ce5a1fd155bc316913e1994ac966cdfd914882
MartinMSalas <ingmartinmsalas@gmail.com>
2026-05-27 21:12:07 -0300

    converter<NullableBDC>:impl

---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/csv/converter/NullableBigDecimalConverter.java
@@ -0,0 +1,27 @@
+package com.esparta.spring7restmvcresourceserver.csv.converter;
+
+import com.opencsv.bean.AbstractBeanField;
+
+import java.math.BigDecimal;
+
+/*
+ * Author: M
+ * Date: 05-Feb-26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+public class NullableBigDecimalConverter
+        extends AbstractBeanField<BigDecimal, String> {
+
+    @Override
+    protected BigDecimal convert(String value) {
+        if (value == null || value.isBlank() || value.equalsIgnoreCase("NA")) {
+            return null;
+        }
+        try {
+            return new BigDecimal(value.trim());
+        } catch (NumberFormatException ex) {
+            return null;
+        }
+    }
+}
