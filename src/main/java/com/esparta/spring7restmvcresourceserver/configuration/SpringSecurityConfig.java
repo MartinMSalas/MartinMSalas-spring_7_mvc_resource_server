62d116b6275afdc866deec37d107d137cd67e347
MartinMSalas <ingmartinmsalas@gmail.com>
2026-05-29 18:14:20 -0300

    sec<SpringSec>:impl

---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/configuration/SpringSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.esparta.spring7restmvcresourceserver.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+/*
+ * Author: M
+ * Date: 16-Feb-26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+@Configuration
+public class SpringSecurityConfig {
+
+    /*
+    // Configure to disable CSRF for API endpoints and allow basic authentication for testing purposes
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        // Disable CSRF for API endpoints
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+
+    }
+    */
+    // Enable OAuth2 Resource Server support with JWT validation for API endpoints
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+}
