f9a956a890de90c4edf46cf41e8a9fe595ceda64
MartinMSalas <ingmartinmsalas@gmail.com>
2026-06-01 20:39:23 -0300

    test<MySQLIT>:impl

---
+++ src/test/java/com/esparta/spring7restmvcresourceserver/repositories/MySqlIT.java
@@ -0,0 +1,55 @@
+package com.esparta.spring7restmvcresourceserver.repositories;
+
+import com.esparta.spring7restmvcresourceserver.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/*
+ * Author: m
+ * Date: 3/2/26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("localmysql")
+public class MySqlIT {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:9.2");
+
+
+//    @DynamicPropertySource
+//    static void mySqlProperties(DynamicPropertyRegistry registry){
+//        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+//        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+//        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+//    }
+
+
+//    @Autowired
+//    DataSource dataSource;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<Beer> beers = beerRepository.findAll();
+
+        assertThat(beers.size()).isGreaterThan(0);
+    }
+
+
+}
