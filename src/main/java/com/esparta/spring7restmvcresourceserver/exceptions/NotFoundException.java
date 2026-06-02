7670179618095ff2b8cbd22d0c54d9d584662396
MartinMSalas <ingmartinmsalas@gmail.com>
2026-06-02 11:42:01 -0300

    excep<NotFound>:impl

---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/exceptions/NotFoundException.java
@@ -0,0 +1,35 @@
+package com.esparta.spring7restmvcresourceserver.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/*
+ * Author: m
+ * Date: 27/1/26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException() {
+
+    }
+
+    public NotFoundException(String message){
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause){
+        super(cause);
+    }
+
+    public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace){
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
