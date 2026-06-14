0104b65edb58ce965cecb22caf4754ffbd2b9ef1
MartinMSalas <ingmartinmsalas@gmail.com>
2026-06-14 11:40:22 -0300

    feat<Service>:impl

---
+++ src/main/java/com/esparta/spring7restmvcresourceserver/services/GreetingService.java
@@ -0,0 +1,11 @@
+package com.esparta.spring7restmvcresourceserver.services;
+
+/*
+ * Author: M
+ * Date: 23-Jan-26
+ * Project Name: spring-7-rest-mvc-resource-server
+ * Description: beExcellent
+ */
+public interface GreetingService {
+    String sayGreeting();
+}
