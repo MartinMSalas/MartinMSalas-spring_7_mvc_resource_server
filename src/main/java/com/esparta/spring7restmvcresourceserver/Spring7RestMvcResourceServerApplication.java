40a65205ac846241a2b100aebd478eaf43276e09
MartinMSalas <ingmartinmsalas@gmail.com>
2026-06-16 17:36:04 -0300

    fix<Auditing>:impl

--- src/main/java/com/esparta/spring7restmvcresourceserver/Spring7RestMvcResourceServerApplication.java
+++ src/main/java/com/esparta/spring7restmvcresourceserver/Spring7RestMvcResourceServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-//@EnableJpaAuditing
+@EnableJpaAuditing
 public class Spring7RestMvcResourceServerApplication {
 
 	public static void main(String[] args) {
